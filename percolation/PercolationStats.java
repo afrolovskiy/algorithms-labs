c940a2f07b0f33f6cf482db2bfe77625bb438f73
a.frolovskiy <a.frolovskiy@corp.mail.ru>
2015-07-19 20:03:07 +0300

    fixed DM_BOXED_PRIMITIVE_FOR_PARSING

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -60,8 +60,8 @@ public class PercolationStats {
             throw new IllegalArgumentException();
         }
 
-        int N = Integer.valueOf(args[0]);
-        int T = Integer.valueOf(args[1]);
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
 
         PercolationStats ps = new PercolationStats(N, T);
 
