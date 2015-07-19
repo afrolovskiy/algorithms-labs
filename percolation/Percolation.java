2863ebd677cb4ab16b3f8413199fd825b9bf3780
a.frolovskiy <a.frolovskiy@corp.mail.ru>
2015-07-19 18:22:12 +0300

    initialize percolation lab

---
+++ percolation/Percolation.java
@@ -0,0 +1,83 @@
+public class Percolation {
+    private int size;
+    private boolean[][] opennedSites;
+    private WeightedQuickUnionUF uf;
+    private int virtTopSite, virtBottomSite;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        size = N;
+ 
+        // all sites blocked by default
+        opennedSites = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                opennedSites[i][j] = false;
+            }
+        }
+        
+        int siteCount = size * size;
+        uf = new WeightedQuickUnionUF(siteCount + 2);  // + 2 virtial sites
+        virtTopSite = siteCount;
+        virtBottomSite = siteCount + 1;
+    };
+
+    private int getLinearIndex(int i, int j) {
+        return (i - 1) * size + j - 1;
+    };
+    
+    private void validate(int i) {
+        if (i < 1 || i > size) {
+            throw new IndexOutOfBoundsException();
+        }
+    };
+
+    public void open(int i, int j) {
+        if (isOpen(i, j)) {
+            return;
+        }
+
+        opennedSites[i-1][j-1] = true;
+ 
+        int idx = getLinearIndex(i, j);
+ 
+        if (i == 1) {
+            uf.union(idx, virtTopSite);
+        } else if (i == size) {
+            uf.union(idx, virtBottomSite);
+        }
+        
+        if (i > 1 && isOpen(i - 1, j)) {
+            uf.union(getLinearIndex(i - 1, j), idx);
+        }
+        
+        if (i < size && isOpen(i + 1, j)) {
+            uf.union(getLinearIndex(i + 1, j), idx);
+        }
+        
+        if (j > 1 && isOpen(i, j - 1)) {
+            uf.union(getLinearIndex(i, j - 1), idx);
+        }
+        
+        if (j < size && isOpen(i, j + 1)) {
+            uf.union(getLinearIndex(i, j + 1), idx);
+        }
+    }
+ 
+    public boolean isOpen(int i, int j) {
+        validate(i);
+        validate(j);
+        return opennedSites[i-1][j-1];
+    }
+
+    public boolean isFull(int i, int j) {
+        return isOpen(i, j) && uf.connected(getLinearIndex(i, j), virtTopSite);
+    }
+
+    public boolean percolates() {
+        return uf.connected(virtTopSite, virtBottomSite);
+    }
+};
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,73 @@
+public class PercolationStats {
+    private double[] thresholds;
+    private int size, retries, siteCount;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        size = N;
+        retries = T;
+        siteCount = size * size;
+ 
+        thresholds = new double[retries];
+        for (int i = 0; i < retries; i++) {
+            thresholds[i] = calcThreshold();
+        }
+    }
+    
+    private double calcThreshold() {
+        int count = 0;
+        Percolation percolation = new Percolation(size);
+        while (!percolation.percolates()) {
+            count++;
+            openRandomSite(percolation);
+        }
+        return ((double) count) / siteCount;
+    };
+
+    private void openRandomSite(Percolation percolation) {
+        int i, j;
+        do {
+            i = StdRandom.uniform(size) + 1;
+            j = StdRandom.uniform(size) + 1;
+        } while (percolation.isOpen(i, j));
+        percolation.open(i, j);
+    };
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (1.96 * Math.sqrt(stddev()) / Math.sqrt(retries));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+         return mean() + (1.96 * Math.sqrt(stddev()) / Math.sqrt(retries));
+    }
+    
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException();
+        }
+
+        Integer N = Integer.valueOf(args[0]);
+        Integer T = Integer.valueOf(args[1]);
+
+        PercolationStats ps = new PercolationStats(N, T);
+
+        StdOut.printf("mean                    = " + ps.mean() + "\n");
+        StdOut.printf("stddev                  = " + ps.stddev() + "\n");
+        StdOut.printf("95%% confidence interval = " + ps.confidenceLo() + " "
+                      + ps.confidenceHi() + "\n");
+    };
+};
