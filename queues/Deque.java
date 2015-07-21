d369154c24a869b4e42ba1e2abe8d8902b2cfa0f
a.frolovskiy <a.frolovskiy@corp.mail.ru>
2015-07-21 23:40:24 +0300

    implemented deque

---
+++ queues/Deque.java
@@ -0,0 +1,154 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node first = null;
+    private Node last = null;
+    private int count = 0;
+
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node prev;
+    }
+
+    public Deque() { }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void addFirst(Item item) {
+        validateItem(item);
+        Node newFirst = new Node();
+        newFirst.item = item;
+        newFirst.next = first;
+        newFirst.prev = null;
+        first = newFirst;
+        count++;
+
+    }
+
+    public void addLast(Item item) {
+        validateItem(item);
+        Node newLast = new Node();
+        newLast.item = item;
+        newLast.next = null;
+        newLast.prev = last;
+        last = newLast;
+        count++;
+    }
+
+    private void validateItem(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    public Item removeFirst() {
+        validateSize();
+
+        Node nextItem = first.next;
+        if (nextItem != null) {
+            nextItem.prev = null;
+        }
+        Node oldFirst = first;
+        oldFirst.next = null;
+        first = nextItem;
+        count--;
+
+        return oldFirst.item;
+    }
+
+    public Item removeLast() {
+        validateSize();
+
+        Node prevItem = last.prev;
+        if (prevItem != null) {
+            prevItem.next = null;
+        }
+        Node oldLast = last;
+        oldLast.prev = null;
+        last = prevItem;
+        count--;
+
+        return oldLast.item;
+    }
+
+    private void validateSize() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        public boolean hasNext() {
+            return !isEmpty();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            return removeFirst();
+        }
+    }
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<Integer>();
+
+        assert deque.isEmpty();
+
+        deque.addFirst(1);
+        assert !deque.isEmpty();
+
+        assert deque.removeFirst() == 1;
+        assert deque.isEmpty();
+
+        deque.addFirst(2);
+        assert !deque.isEmpty();
+
+        assert deque.removeLast() == 2;
+        assert deque.isEmpty();
+
+        deque.addLast(3);
+        assert !deque.isEmpty();
+
+        assert deque.removeFirst() == 3;
+        assert deque.isEmpty();
+
+        deque.addLast(4);
+        assert !deque.isEmpty();
+
+        assert deque.removeLast() == 4;
+        assert deque.isEmpty();
+
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(3);
+        deque.addLast(4);
+
+        assert deque.removeFirst() == 1;
+        assert deque.removeFirst() == 2;
+        assert deque.removeFirst() == 3;
+        assert deque.removeFirst() == 4;
+
+        deque.addLast(3);
+        deque.addLast(4);
+        deque.addFirst(2);
+        deque.addFirst(1);
+
+        assert deque.removeLast() == 4;
+        assert deque.removeLast() == 3;
+        assert deque.removeLast() == 2;
+        assert deque.removeLast() == 1;
+    }
+}
