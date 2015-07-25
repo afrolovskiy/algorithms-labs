6e34e4545f5900188938e2a7bac3b54c86169063
a.frolovskiy <a.frolovskiy@corp.mail.ru>
2015-07-25 20:49:50 +0300

    implement subset

---
+++ queues/Subset.java
@@ -0,0 +1,22 @@
+import java.util.Iterator;
+
+public class Subset {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException();
+        }
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        int count = 0;
+        while (count < k && !StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+            count++;
+        };
+
+        Iterator<String> iter = queue.iterator();
+        while (iter.hasNext()) {
+            StdOut.printf(iter.next() + "\n");
+        }
+    }
+}
