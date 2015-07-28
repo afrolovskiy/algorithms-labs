4307788472077c90024b92c0b8497082163e41d0
a.frolovskiy <a.frolovskiy@corp.mail.ru>
2015-07-28 22:43:28 +0300

    implement compareTo

--- colinear/Point.java
+++ colinear/Point.java
@@ -17,25 +17,19 @@ public class Point implements Comparable<Point> {
     // compare points by slope
     public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
 
-    private final int x;                              // x coordinate
-    private final int y;                              // y coordinate
+    private final int x;
+    private final int y;
 
-    // create the point (x, y)
     public Point(int x, int y) {
-        /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
 
-    // plot this point to standard drawing
     public void draw() {
-        /* DO NOT MODIFY */
         StdDraw.point(x, y);
     }
 
-    // draw line between this point and that point to standard drawing
     public void drawTo(Point that) {
-        /* DO NOT MODIFY */
         StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
@@ -44,15 +38,24 @@ public class Point implements Comparable<Point> {
         /* YOUR CODE HERE */
     }
 
-    // is this point lexicographically smaller than that one?
-    // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        if (this.y < that.y) {
+            return -1;
+        }
+
+        if (this.y == that.y) {
+            if (this.x < that.x) {
+                return -1;
+            }
+            if (this.x == that.x) {
+                return 0;
+            }
+        }
+
+        return 1;
     }
 
-    // return string representation of this point
     public String toString() {
-        /* DO NOT MODIFY */
         return "(" + x + ", " + y + ")";
     }
 
